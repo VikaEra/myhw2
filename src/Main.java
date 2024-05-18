4898d0f5d649ac61745a06e74dccd22af880f92c
VikaEra <vikastav26@yandex.ru>
2024-05-18 10:51:38 +0300

    Main.java added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 13676;
-        int oneBonusMile = 20;
-        int bonusMiles = ticketPrice / oneBonusMile;
+        int ticketPrice = 1376; // стоимость билета
+        int oneBonusMile = 20; // хранения количества рублей для одной бонусной мили
+        int bonusMiles = ticketPrice / oneBonusMile; // количество начисленных миль за купленный билет
         System.out.println("Количество начисленных миль за купленный билет: " + bonusMiles);
     }
 }
